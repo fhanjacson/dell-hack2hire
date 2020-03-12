bec9d685016cfdaba3fb9f01ae801a12e7b19ee9
JJLooi <jjlooi.ljj@gmail.com>
2020-03-12 14:52:26 +0800

    JJ workable FOR LOOP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { AdminComponent } from './pages/admin/admin.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+import { AdminComponent } from "./pages/admin/admin.component";
+import { SidePanelComponent } from "./testPanel/side-panel/side-panel.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'admin', component: AdminComponent }
+  { path: "", component: HomeComponent },
+  { path: "admin", component: AdminComponent },
+  { path: "sideNav", component: SidePanelComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidePanelComponent } from './sidePanel/side-panel/side-panel.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SidePanelComponent } from "./testPanel/side-panel/side-panel.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SidePanelComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SidePanelComponent],
+  imports: [BrowserModule, AppRoutingModule, MatSidenavModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
