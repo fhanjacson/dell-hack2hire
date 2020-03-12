0d3d769c497394b2b0c5d57b3a6ca16fcf36e90b
Fhan Jacson <fhan.jacson@gmail.com>
2020-03-12 20:15:50 +0800

    almost finish dragging

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -9,38 +9,45 @@ import { CdkDragDrop, transferArrayItem, moveItemInArray, copyArrayItem } from '
 export class AdminComponent implements OnInit {
 
   listOfWidgets = [1, 2, 3, 4, 5];
-  layouts = [[[1], [3]], [[2], []], [[]]];
+  layouts = [[[1], [3]], [[2], [0]], [[0]]];
   // tempArray = [];[]
   isEmpty = true;
 
   addColumn(index: number) {
-    if (this.layouts[index].length < 5) {
-      this.layouts[index].push([]);
+    if (this.layouts[index].length < 4) {
+      this.layouts[index].push([0]);
     }
-    console.log(this.layouts);
   }
 
   addRow() {
-    this.layouts.push([]);
-    console.log(this.layouts);
-
+    this.layouts.push([[0]]);
   }
 
-  drop(event: CdkDragDrop<any>) {
+  drop(event: CdkDragDrop<any>, asd: number) {
     // console.log(event.container);
-    console.log([[]]);
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    console.log('asd' + asd);
+    // if (event.previousContainer === event.container) {
+    //   // moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    // } else {
+    console.log(event.container.data[asd][0]);
+    if (event.container.data[asd][0] === 0) {
+      console.log('success');
+      // event.container.data[asd] = [event.previousContainer.data[event.previousIndex]];
+
+      if (typeof event.previousContainer.data[event.previousIndex] === 'number') {
+        console.log(event.container.data[asd]);
+        console.log([event.previousContainer.data[event.previousIndex]]);
+        event.container.data[asd] = [event.previousContainer.data[event.previousIndex]];
+      } else {
+        console.log(event.previousContainer.data[3]);
+        event.container.data[asd] = event.previousContainer.data[3];
+      }
     } else {
-      console.log(event.container.data.length);
-      copyArrayItem(event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex);
+      event.container.data[asd] = [event.previousContainer.data[event.previousIndex]];
 
     }
-
   }
+  // }
 
   constructor() { }
 
