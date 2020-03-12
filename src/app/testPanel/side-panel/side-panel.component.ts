7fc15c31711fbd8126daade32d47eb9be22dd824
JJLooi <jjlooi.ljj@gmail.com>
2020-03-12 19:17:22 +0800

    side nav

--- src/app/testPanel/side-panel/side-panel.component.ts
+++ src/app/testPanel/side-panel/side-panel.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  copyArrayItem
+} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: "app-side-panel",
   templateUrl: "./side-panel.component.html",
   styleUrls: ["./side-panel.component.scss"]
 })
-// export class SidePanelComponent implements OnInit {
-export class SidePanelComponent {
-  showFiller = true;
+export class SidePanelComponent implements OnInit {
+  showFiller = false;
 
-  // constructor() {}
+  //drag drop testing
+  numbers: number[] = [];
+  otherNumbers: number[] = [];
 
-  // ngOnInit(): void {}
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      this.numbers.push(i);
+    }
+  }
+
+  ngOnInit(): void {}
+
+  drop(event: CdkDragDrop<any>) {
+    if (event.previousContainer !== event.container) {
+      copyArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      event.container;
+    } else {
+      moveItemInArray(this.numbers, event.previousIndex, event.currentIndex);
+    }
+  }
 }
